0e2b200c2bf467a69a7f2bf53063e8d370e27ab4
zgliuwudi <zgliuwudi@163.com>
2021-07-04 04:47:55 +0800

    init all modules

--- content/src/main/java/com/taiwarm/cloud/content/ContentApplication.java
+++ content/src/main/java/com/taiwarm/cloud/content/ContentApplication.java
@@ -2,8 +2,10 @@ package com.taiwarm.cloud.content;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ContentApplication {
 
     public static void main(String[] args) {
